038f6d720262a1d49f0aedd861044794fe8e0346
Peter Tam <peter.tam@sjsu.edu>
2024-10-28 18:27:27 -0700

    Implemented multistreaming via URL query

--- src/app/(browse)/[username]/page.tsx
+++ src/app/(browse)/[username]/page.tsx
@@ -3,16 +3,19 @@ import {notFound} from "next/navigation";
 import {isFollowingUser} from "@/lib/follow-service";
 import {isBlockedByUser} from "@/lib/block-service";
 import {StreamPlayer} from "@/components/stream-player";
+import {MultiStreamPlayer} from "@/components/stream-player/multi-stream";
 
 interface UserPageProps {
     params: {
         username: string;
     }
+    searchParams?: {
+        multi: string;
+    }
 }
 
-const UserPage = async ({params}: UserPageProps) => {
+const UserPage = async ({params, searchParams}: UserPageProps) => {
     const user = await getUserByUsername(params.username)
-
     if (!user || !user.stream) notFound()
 
     const isFollowing = await isFollowingUser(user.id)
@@ -20,8 +23,24 @@ const UserPage = async ({params}: UserPageProps) => {
 
     if (isBlocked) notFound()
 
+    const multiParam = searchParams?.multi
+    let additionalUsernames: (string)[];
+    if (multiParam) {
+        additionalUsernames = multiParam.split(',')
+    } else {
+        additionalUsernames = []
+    }
+
+    const additionalUsers = await Promise.all(
+        additionalUsernames.map((username) => getUserByUsername(username))
+    );
+    
+    if (additionalUsers.every(user => user === null)) {
+        return <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowing}/>
+    }
+
     return (
-        <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowing}/>
+        <MultiStreamPlayer user={user} users={additionalUsers} stream={user.stream} isFollowing={isFollowing}/>
     )
 }
 
---
+++ src/components/stream-player/multi-stream.tsx
@@ -0,0 +1,104 @@
+"use client"
+
+import {useViewerToken} from "@/hooks/use-viewer-token";
+import {LiveKitRoom} from "@livekit/components-react";
+import {Video, VideoSkeleton} from "@/components/stream-player/video";
+import {useChatSidebar} from "@/store/use-chat-sidebar";
+import {cn} from "@/lib/utils";
+import {Chat, ChatSkeleton} from "@/components/stream-player/chat";
+import {ChatToggle} from "@/components/stream-player/chat-toggle";
+import {Header, HeaderSkeleton} from "@/components/stream-player/header";
+import {InfoCard} from "@/components/stream-player/info-card";
+import {AboutCard} from "@/components/stream-player/about-card";
+
+type CustomStream = {
+    id: string
+    isChatEnabled: boolean
+    isChatDelayed: boolean
+    isChatFollowersOnly: boolean
+    isLive: boolean
+    thumbnailUrl: string | null
+    name: string
+}
+
+type CustomUser = {
+    id: string
+    username: string
+    bio: string | null
+    stream: CustomStream | null
+    imageURL: string
+    _count: {
+        followedBy: number
+    }
+}
+
+interface StreamPlayerProps {
+    user: CustomUser
+    users: (CustomUser | null)[]
+    stream: CustomStream
+    isFollowing: boolean
+}
+
+export const MultiStreamPlayer = ({user, users, stream, isFollowing}: StreamPlayerProps) => {
+    const {token, name, identity} = useViewerToken(user.id)
+    const {collapsed} = useChatSidebar((state) => state)
+
+    if (!token || !name || !identity) return <MultiStreamPlayerSkeleton/>
+
+    return (
+        <>
+            {collapsed && (
+                <div className={"hidden lg:block fixed top-[100px] right-2 z-50"}>
+                    <ChatToggle/>
+                </div>
+            )}
+            <LiveKitRoom serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL} token={token}
+                         className={cn("grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+                             collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2")}>
+                <div
+                    className={"space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10"}>
+                    <div className={"grid grid-cols-2 gap-2"}>
+                        <Video hostName={user.username} hostIdentity={user.id}/>
+                        {users[0] && (
+                            <Video hostName={users[0].username} hostIdentity={users[0].id} />
+                        )}
+                        {users[1] && (
+                            <Video hostName={users[1].username} hostIdentity={users[1].id} />
+                        )}
+                        {users[2] && (
+                            <Video hostName={users[2].username} hostIdentity={users[2].id} />
+                        )}
+
+                    </div>
+                    
+                    <Header hostName={user.username} hostIdentity={user.id} viewerIdentity={identity}
+                            imageURL={user.imageURL} isFollowing={isFollowing} name={stream.name}/>
+                    <InfoCard hostIdentity={user.id} viewerIdentity={identity} name={stream.name}
+                              thumbnailUrl={stream.thumbnailUrl}/>
+                    <AboutCard hostName={user.username} hostIdentity={user.id} viewerIdentity={identity} bio={user.bio}
+                               followedByCount={user._count.followedBy}/>
+                </div>
+                <div className={cn("col-span-1", collapsed && "hidden")}>
+                    <Chat viewerName={name} hostName={user.username} hostIdentity={user.id} isFollowing={isFollowing}
+                          isChatEnabled={stream.isChatEnabled} isChatDelayed={stream.isChatDelayed}
+                          isChatFollowersOnly={stream.isChatFollowersOnly}/>
+                </div>
+            </LiveKitRoom>
+        </>
+    )
+}
+
+export const MultiStreamPlayerSkeleton = () => {
+    return (
+        <div className={"grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full "}>
+            <div
+                className={"space-y-2 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10"}>
+                <VideoSkeleton/>
+                <HeaderSkeleton/>
+            </div>
+            <div className={"col-span-1 bg-background"}>
+                <ChatSkeleton/>
+            </div>
+        </div>
+    )
+}
